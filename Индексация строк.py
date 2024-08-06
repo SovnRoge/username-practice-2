6d60d74aca5e614435b8bcf4aa218ece5a3f420d
SovnRoge <nefedov-9191@mail.ru>
2024-08-06 11:23:26 +0500

    Add files via upload

---
+++ Индексация строк.py
@@ -0,0 +1,8 @@
+name_1 = 'это моя строка'
+name_2 = 'это новая строка'
+print('1) ' + name_1[0])
+print('2) ' + name_1[-1])
+print('3) ' + name_1[2:4])
+print('4) ' + name_1[::-1])
+print('5)', + len(name_1))
+print('6) ' + name_2 + name_1)
