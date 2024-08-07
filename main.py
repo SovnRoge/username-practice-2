4e24d9d594f65438bfc850f577fd5af34dd4825f
SovnRoge <nefedov-9191@mail.ru>
2024-08-07 23:33:13 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+# 1st_program
+print((9 ** 0.5) * 5)
+# 2nd_program
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3rd_program
+print(2*2+2)
+print((2+2)*2)
+print((2*2+2) == (2+2)*2)
+# 4th_program
+name = ((float('123.456')*10) % 10)
+print(int(name))
