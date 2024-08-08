0cc46536ca5aab9d990ea14e1408a4b90e53a163
SovnRoge <nefedov-9191@mail.ru>
2024-08-08 23:46:53 +0500

    Add files via upload

---
+++ Переменные_2.py
@@ -0,0 +1,6 @@
+homework_qt = 12
+hours_qt = 1.5
+name_of_class='Python'
+time_for_task=(hours_qt/homework_qt)
+print('Курс:',name_of_class,', ''всего задач:',homework_qt,', ''затрачено часов:',hours_qt,', '
+    'среднее время выполнения:',time_for_task,'.',sep=(''))
