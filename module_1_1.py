c704e144789aa27e6684741cdd76b714ab70bd9c
SovnRoge <nefedov-9191@mail.ru>
2024-08-22 12:50:01 +0500

    Add files via upload

---
+++ module_1_1.py
@@ -0,0 +1,6 @@
+example_1 = 'АльфаЦентавра'
+print('1) ' + example_1[0])
+print('2) ' + example_1[-1])
+print('3) ' + example_1[6:13])
+print('4) ' + example_1[::-1])
+print('5) ' + example_1[1:12:2])
